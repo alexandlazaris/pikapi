5ec37e171041b0d07d018f86e98b2d47dea48958
alexandlazaris <alex.lazaris617@gmail.com>
2019-04-23 23:47:25 +1000

    Output pokemon found to a txt file + add more content to ReadMe

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # pikapi
 
-```API: https://pokeapi.co/```
+![pokemon](http://images6.fanpop.com/image/polls/1192000/1192334_1363471326113_full.jpg)
 
-Features:
+API: ```https://pokeapi.co/```
+End goal: Pokemon SMS news
+
+Features (so far):
 
 * Display a random pokemon by its pokedex number
+* Outputs result to a ```.txt``` file
 
--- pokemonByid.py
+++ pokemonByid.py
@@ -1,17 +1,23 @@
 import requests, json, pprint, os, sys, random
 
+def runAPI():
+    urlPokemon = 'https://pokeapi.co/api/v2/pokemon/'
+    randomNumber = random.randint(1, 386)
+    urlPokemonById = urlPokemon + str(randomNumber)
 
-urlPokemon = 'https://pokeapi.co/api/v2/pokemon/'
-randomNumber = random.randint(1, 386)
-urlPokemonById = urlPokemon + str(randomNumber)
+    response = requests.get(urlPokemonById)
+    pokeId = 'Pokedex entry > ' + str(response.json()['id'])
+    pokeName = 'Pokemon name > ' + str(response.json()['name'])
+    pokemonInfo = [pokeId, pokeName]
+    for i in response.json()['types']:
+        str(pokemonInfo.append('Pokemon type > ' + i['type']['name']))
+    pokeSprite = str(response.json()['sprites']['front_default'])
+    pokemonInfo.append('Pokemon sprite > ' + pokeSprite)
+    for data in pokemonInfo:
+        print (data)
 
-response = requests.get(urlPokemonById)
-pokeId = 'Pokedex entry > ' + str(response.json()['id'])
-pokeName = 'Pokemon name > ' + str(response.json()['name'])
-pokemonInfo = [pokeId, pokeName]
-for i in response.json()['types']:
-    str(pokemonInfo.append('Pokemon type > ' + i['type']['name']))
-pokeSprite = str(response.json()['sprites']['front_default'])
-pokemonInfo.append('Pokemon sprite > ' + pokeSprite)
-for data in pokemonInfo:
-    print (data)
+    f=open("./data.txt","w")
+    for data in pokemonInfo:
+         f.write(data + "\n")
+    f.close()
+runAPI()
